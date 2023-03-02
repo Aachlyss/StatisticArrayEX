0a7c96ef14548eeb2620cf56ce7026475f5d6979
Aachlyss <ivanangelov05@gmail.com>
2023-03-02 11:36:34 +0200

    Commit

---
+++ Tests/UnitTest1.cs
@@ -0,0 +1,56 @@
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void ConstructorShouldInitializeCorrectly()
+        {
+            
+        }
+        [Test]
+        public void ModelShouldThrowArgExWhenNameIsNull()
+        {
+
+        }
+        [Test]
+        public void MakeShouldThrowArgExWhenNameIsNull()
+        {
+
+        }
+        [Test]
+        public void FuelConsumptionShouldThrowArgExWhenIsBellowZero()
+        {
+
+        }
+        [Test]
+        public void FuelConsumptionShouldThrowArgExWhenIsZero()
+        {
+
+        }
+        [Test]
+        public void FuelCapacityShouldThrowArgExWhenIsZero()
+        {
+
+        }
+        [Test]
+        public void FuelCapacityShouldThrowArgExWhenIsBellowZero()
+        {
+
+        }
+        [Test]
+        [TestCase()]
+        //TO DO
+        public void ValidateAllProperties(string make, string model, double fuelConsumption, double
+        fuelCapacity)
+        {
+            //TO DO
+        }
+    }
+}
